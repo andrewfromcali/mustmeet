8352e568d8f7abdf8c15a264964a1fc625559158
aa <andrew@geni.com>
2009-01-23 18:20:47 -0800

    more

--- app/controllers/facebook_controller.rb
+++ app/controllers/facebook_controller.rb
@@ -1,7 +1,13 @@
 class FacebookController < ApplicationController
+  before_filter :require_facebook_login
   
   def index
+    response = fbsession.users_getInfo(:uids => fbsession.friends_get.uid_list, 
+                                       :fields => [:name, :pic_small])
+    @list = response.user_list
+  end
   
+  def test
   end
   
 end
