5c1e2c1dff1483087c305a4c13353691b7b269bd
aa <andrew@geni.com>
2009-01-25 11:01:24 -0800

    fixed

--- mustmeet/views.py
+++ mustmeet/views.py
@@ -7,8 +7,5 @@ import facebook.djangofb as facebook
 
 @facebook.require_login()
 def main(request):
-  response = fbsession.users_getInfo(:uids => fbsession.friends_get.uid_list, 
-                                       :fields => [:name, :pic_square])
-  @list = response.user_list
-  name = request.facebook.users.getInfo([request.facebook.uid], ['first_name'])[0]['first_name']
-  return render_to_response('main/main.html', {'name': name})
+  list = request.facebook.users.getInfo(request.facebook.friends.get(), ['name','pic_square'])
+  return render_to_response('main/main.html', {'list': list})
